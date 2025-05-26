53b4154c560f8cfc95032f380264c6be904dce8b
johanREGIS <johanjregis@gmail.com>
2025-05-27 01:21:30 +0200

    Page d'accueil basique

--- src/app/components/flight-controller/flight-controller.component.ts
+++ src/app/components/flight-controller/flight-controller.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FlightControllerService } from '../../services/flight-controller/flight-controller.service';
+import { FlightController } from '../../models/flight-controller/flight-controller';
 
 @Component({
   selector: 'app-flight-controller',
@@ -6,6 +8,42 @@ import { Component } from '@angular/core';
   templateUrl: './flight-controller.component.html',
   styleUrl: './flight-controller.component.css'
 })
-export class FlightControllerComponent {
+export class FlightControllerComponent implements OnInit {
 
+  flightControllers:FlightController[] = [];
+  vitesse:number = 0;
+  altitude:number = 0;
+  latitude:number = 0;
+  longitude:number = 0;
+
+  constructor(private flightControllerService:FlightControllerService) {}
+  
+  ngOnInit(): void {
+    this.getFlightControllers();
+  }
+
+  getFlightControllers() {
+    this.flightControllerService.getAllFlightControllers().subscribe({  
+      next:(data) => {
+        this.flightControllers = data as FlightController[];
+      },
+      error: (err) => console.error('Erreur de chargement des techniciens', err)
+    });
+  }
+
+  changeVitesse(controller:FlightController) {
+    
+  }
+
+  changeAltitude(direction: string) {
+    
+  }
+
+  changeLatitude(direction: string) {
+    
+  }
+
+  changeLongitude(direction: string) {
+    
+  }
 }
